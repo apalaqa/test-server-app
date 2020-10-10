399e7f2f52539230ab1578d0104f7f723baedea0
Artur Palamarchuk <artur.palamarchuk@dswiss.com>
2020-10-10 18:11:48 +0300

    added other files

---
+++ src/migrations/1600950210287-startSchema.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class startSchema1600950210287 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
